7ff87f4ef967ff41355a490ac6cfa8046c374d63
Bhupinder Sindhwani <bsindhwani@google.com>
2023-11-13 17:02:13 -0500

    longest_substring refactored

--- sliding_window/longest_substring.py
+++ sliding_window/longest_substring.py
@@ -9,26 +9,43 @@ def longest_substring(s: str) -> int:
     Returns:
         length of the longest substring without repeating characters
     """
-    if not s:
-        return 0
+    # # Initial Solution
+    # if not s:
+    #     return 0
+    #
+    # left, right = 0, 0
+    # char_set = set()
+    #
+    # max_len_substring = 0
+    #
+    # while right < len(s):
+    #     if s[right] not in char_set:
+    #         char_set.add(s[right])
+    #     else:
+    #         max_len_substring = max(max_len_substring, right - left)
+    #         while s[left] != s[right]:
+    #             char_set.remove(s[left])
+    #             left += 1
+    #         left += 1
+    #     right += 1
+    #
+    # return max(max_len_substring, right - left)
 
-    left, right = 0, 0
-    char_set = set()
+    # Refactored Solution
+    start, end = 0, 0
+    max_len = 0
+    win_set = set()
 
-    max_len_substring = 0
-
-    while right < len(s):
-        if s[right] not in char_set:
-            char_set.add(s[right])
+    while end < len(s):
+        if s[end] not in win_set:
+            win_set.add(s[end])
+            end += 1
         else:
-            max_len_substring = max(max_len_substring, right - left)
-            while s[left] != s[right]:
-                char_set.remove(s[left])
-                left += 1
-            left += 1
-        right += 1
+            max_len = max(max_len, end - start)
+            win_set.remove(s[start])
+            start += 1
 
-    return max(max_len_substring, right - left)
+    return max(max_len, end - start)
 
 
 if __name__ == "__main__":
