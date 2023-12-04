c288bd562d9181c329151a23a32d42aecdbe8882
Bhupinder Sindhwani <bsindhwani@google.com>
2023-12-04 06:19:27 -0500

    house robber - refactored

--- dynamic_programming/house_robber.py
+++ dynamic_programming/house_robber.py
@@ -25,12 +25,37 @@ def rob(nums: List[int]) -> int:
     #
     # return max(nums[0] + rob(nums[2:]), rob(nums[1:]))
 
-    # Solution using dynamic programming
-    one, two = 0, 0
-    for num in nums:
-        temp = max(num + one, two)
-        one, two = two, temp
-    return two
+    # # Solution using dynamic programming
+    # one, two = 0, 0
+    # for num in nums:
+    #     temp = max(num + one, two)
+    #     one, two = two, temp
+    # return two
+
+    # Solution using backtracking and dynamic programming
+    cache = {}
+
+    def backtrack(idx, action):
+
+        # Base condition
+        if idx >= len(nums):
+            return 0
+
+        # Check cache
+        if (idx, action) in cache:
+            return cache[(idx, action)]
+
+        # do nothing
+        nothing = backtrack(idx + 1, action)
+
+        # rob
+        if action == 'rob':
+            robbing = backtrack(idx + 2, 'rob') + nums[idx]
+            cache[(idx, action)] = max(nothing, robbing)
+
+        return cache[(idx, action)]
+
+    return backtrack(0, 'rob')
 
 
 if __name__ == '__main__':
